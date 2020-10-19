4078036b20aef3f80182946578a6829068bcba63
ryanktoeung <ryanktoeung@users.noreply.github.com>
2020-10-18 21:49:28 -0700

    Created Main Screen and start get and read functions

--- app/src/main/java/com/example/wiki_scraper/MainActivity.kt
+++ app/src/main/java/com/example/wiki_scraper/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.wiki_scraper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val read = findViewById<Button>(R.id.readBtn)
+        val get = findViewById<Button>(R.id.getBtn)
+
+        read.setOnClickListener {
+            val i = Intent(
+                this,
+                readActivity::class.java
+            );         //If user login's with correct info, will redirect to the next page
+            startActivity(i);
+        }
+
+        get.setOnClickListener {
+            val i = Intent(
+                this,
+                getActivity::class.java
+            );         //If user login's with correct info, will redirect to the next page
+            startActivity(i);
+        }
     }
 }
---
+++ app/src/main/java/com/example/wiki_scraper/readActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wiki_scraper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class readActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read)
+    }
+}
